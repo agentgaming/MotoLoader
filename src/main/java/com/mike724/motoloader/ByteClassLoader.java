95a71a6002d223736d2829c5d47b3a3c3990f992
Michael <mike@mike724.com>
2013-05-05 16:46:06 -0400

    Adding dakota's crap

---
+++ src/main/java/com/mike724/motoloader/ByteClassLoader.java
@@ -0,0 +1,29 @@
+package org.mike724.motoloader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dakota
+ * Date: 5/5/13
+ * Time: 2:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ByteClassLoader extends ClassLoader {
+    private final Map<String, byte[]> classDefs;
+
+    public ByteClassLoader(Map<String, byte[]> extraClassDefs) {
+        this.classDefs = new HashMap<String, byte[]>(extraClassDefs);
+    }
+
+    @Override
+    protected Class<?> findClass(final String name) throws ClassNotFoundException {
+        byte[] classBytes = this.classDefs.remove(name);
+        if (classBytes != null) {
+            return defineClass(name, classBytes, 0, classBytes.length);
+        }
+        return super.findClass(name);
+    }
+
+}
---
+++ src/main/java/com/mike724/motoloader/MotoLoader.java
@@ -0,0 +1,56 @@
+package org.mike724.motoloader;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public final class MotoLoader extends JavaPlugin {
+
+    private static MotoLoader instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        try {
+            this.getServer().getPluginManager().registerInterface(BytePluginLoader.class);
+            BytePluginLoader bpl = new BytePluginLoader(this.getServer());
+            bpl.loadPlugin(readFile(new File("test.jar")));
+        } catch(Exception e) {
+
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+
+    public static File getFile0() {
+        return instance.getFile();
+    }
+
+    public static MotoLoader getInstance() {
+        return instance;
+    }
+
+    public static byte[] readFile(File file) throws IOException {
+        // Open file
+        RandomAccessFile f = new RandomAccessFile(file, "r");
+        try {
+            // Get and check length
+            long longlength = f.length();
+            int length = (int) longlength;
+            if (length != longlength)
+                throw new IOException("File size >= 2 GB");
+            // Read file and return data
+            byte[] data = new byte[length];
+            f.readFully(data);
+            return data;
+        } finally {
+            f.close();
+        }
+    }
+}
