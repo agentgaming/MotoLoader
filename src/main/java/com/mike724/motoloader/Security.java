e04c5334035559da994a28dbcfc2602ec963ff85
Dakota <dakota@dasigns.net>
2013-08-08 14:18:26 -0400

    Moved to newer encryption

--- src/main/java/com/mike724/motoloader/Security.java
+++ src/main/java/com/mike724/motoloader/Security.java
@@ -3,33 +3,43 @@ package com.mike724.motoloader;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
 
-@SuppressWarnings("unused")
-class Security {
-    public static String encrypt(String input, String key) {
-        byte[] crypted = null;
+public class Security {
+
+    private static String iv = "ma0pel18dnwlg510";
+
+    public static String decrypt(String code, String SecretKey) throws Exception {
+        IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+        SecretKeySpec keyspec = new SecretKeySpec(SecretKey.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+
+        if (code == null || code.length() == 0)
+            throw new Exception("Empty string");
+
+        byte[] decrypted = null;
+
         try {
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CCB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, skey);
-            crypted = cipher.doFinal(input.getBytes("UTF-8"));
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+            decrypted = cipher.doFinal(Base64.decodeBase64(code));
         } catch (Exception e) {
-            System.out.println(e.toString());
+            throw new Exception("[decrypt] " + e.getMessage());
         }
-        return new String(Base64.encodeBase64(crypted));
+        return new String(decrypted);
     }
+    private static String padString(String source) {
+        char paddingChar = ' ';
+        int size = 16;
+        int x = source.length() % size;
+        int padLength = size - x;
 
-    public static String decrypt(String input, String key) {
-        byte[] output = null;
-        try {
-            SecretKeySpec skey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CCB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, skey);
-            output = cipher.doFinal(Base64.decodeBase64(input));
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        for (int i = 0; i < padLength; i++) {
+            source += paddingChar;
         }
-        return new String(output);
+
+        return source;
     }
 }
